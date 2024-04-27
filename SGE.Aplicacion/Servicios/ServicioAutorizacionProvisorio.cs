ad8adafb449a8df5bf8fa4c275ebca8b43ec07b7
Neftalí Toledo <47830827+neftalito@users.noreply.github.com>
2024-04-27 00:16:13 -0300

    #14 Añadir ServicioAutorizacion

--- SGE.Aplicacion/Servicios/ServicioAutorizacionProvisorio.cs
+++ SGE.Aplicacion/Servicios/ServicioAutorizacionProvisorio.cs
@@ -1,6 +1,12 @@
-﻿namespace SGE.Aplicacion.Servicios;
+﻿using SGE.Aplicacion.Enumerativos;
+using SGE.Aplicacion.Interfaces;
 
-public class ServicioAutorizacionProvisorio
+namespace SGE.Aplicacion.Servicios;
+
+public class ServicioAutorizacionProvisorio : IServicioAutorizacion
 {
-    
+    public bool PoseeElPermiso(int idUsuario, Permiso permiso)
+    {
+        return idUsuario == 1;
+    }
 }
