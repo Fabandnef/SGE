01a3a88ea9df00b2a655b579fddf77d15c22e32f
Fabro <fabro.la22@gmail.com>
2024-04-27 18:21:09 -0300

    #25 Cambios de estado. Servicios.

--- SGE.Aplicacion/Servicios/ServicioActualizacionEstado.cs
+++ SGE.Aplicacion/Servicios/ServicioActualizacionEstado.cs
@@ -1,6 +1,30 @@
-﻿namespace SGE.Aplicacion.Servicios;
+﻿using SGE.Aplicacion.Entidades;
+using SGE.Aplicacion.Enumerativos;
+using SGE.Aplicacion.Interfaces;
 
-public class ServicioActualizacionEstado
+namespace SGE.Aplicacion.Servicios;
+
+public class ServicioActualizacionEstado(IExpedienteRepositorio repositorio, EspecificacionCambioEstado especificacionCambioEstado)
 {
+    public void ActualizarEstado(int idExpediente)
+    {
+        EstadoExpediente? estado = especificacionCambioEstado.DefinirEstado(idExpediente);
+
+        if (estado is null) {
+            return;
+        }
+
+        repositorio.ExpedienteActualizarEstado(idExpediente, estado.Value);
+    }
     
+    public void ActualizarEstado(Tramite tramite)
+    {
+        EstadoExpediente? estado = especificacionCambioEstado.DefinirEstado(tramite);
+
+        if (estado is null) {
+            return;
+        }
+
+        repositorio.ExpedienteActualizarEstado(tramite.ExpedienteId, estado.Value);
+    }
 }
