28d73dfb33c0175550af89848157b6ef1870e6d9
Fabro <fabro.la22@gmail.com>
2024-04-27 18:40:09 -0300

    #27 Adaptación de las bajas de tramites para que actualicen el estado del expediente acorde al último tramite que haya.

--- SGE.Aplicacion/CasosDeUso/TramiteBajaCasoDeUso.cs
+++ SGE.Aplicacion/CasosDeUso/TramiteBajaCasoDeUso.cs
@@ -2,11 +2,12 @@
 using SGE.Aplicacion.Enumerativos;
 using SGE.Aplicacion.Excepciones;
 using SGE.Aplicacion.Interfaces;
+using SGE.Aplicacion.Servicios;
 using SGE.Aplicacion.Validadores;
 
 namespace SGE.Aplicacion.CasosDeUso;
 
-public class TramiteBajaCasoDeUso(ITramiteRepositorio repositorio, IServicioAutorizacion servicioAutorizacion)
+public class TramiteBajaCasoDeUso(ITramiteRepositorio repositorio, IServicioAutorizacion servicioAutorizacion, ServicioActualizacionEstado servicioActualizacionEstado)
 {
     public void Ejecutar(int idTramite, int idUsuarioActual)
     {
@@ -14,10 +15,16 @@ public class TramiteBajaCasoDeUso(ITramiteRepositorio repositorio, IServicioAuto
             throw new RepositorioException("El Id del trámite debe ser mayor a 0.");
         }
         
+        Tramite? e = repositorio.ObtenerTramitePorId(idTramite);
+        if (e == null) {
+            throw new RepositorioException("El trámite no existe.");
+        }
+        
         if (!servicioAutorizacion.PoseeElPermiso(idUsuarioActual, Permiso.ExpedienteBaja)) {
             throw new AutorizacionException("El usuario no tiene permisos para realizar esta acción.");
         }
         
         repositorio.BajaTramite(idTramite);
+        servicioActualizacionEstado.ActualizarEstado(e.ExpedienteId);
     }
 }
